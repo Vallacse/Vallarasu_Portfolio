8f3d466b150948463f68e6fe6dd99548c22b07aa
Vallarasu <vallacse@.com>
2024-02-10 23:40:53 +0530

    updated about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,12 +9,11 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Vallacse </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Salem, Tamilnadu, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a software developer at C-Out.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed ME(CSE) at Anna University.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Vallacsejit</footer>
+          <footer className="blockquote-footer">Vallacse</footer>
         </blockquote>
       </Card.Body>
     </Card>
